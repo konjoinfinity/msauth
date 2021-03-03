4e35814725bf3015415a9e758c71676c92939716
Wesley Scholl <clients@konjoweb.com>
2021-03-02 22:58:08 -0500

    testing possible fixes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
-import Api from './api/index'
+import Api from './api'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Auth from './Auth';
